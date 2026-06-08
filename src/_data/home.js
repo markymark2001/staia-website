ac48f5b4a2589decae85bcb747823fd14f7bfdc5
markymark2001 <mark.vasilyev.beats@gmail.com>
2026-06-08 21:59:27 +0100

    Fix homepage review dates

--- src/_data/home.js
+++ src/_data/home.js
@@ -29,21 +29,21 @@ export default {
         {
           initials: "H",
           name: "Haleymeshale",
-          handle: "Haleymeshale",
+          handle: "Jun 4, 2026",
           title: "LOVE IT",
           copy: "Best thing I did this year was download this app. I've never felt so seen. It's like talking to a friend who just gets your soul so deeply."
         },
         {
           initials: "P",
           name: "Patron7370",
-          handle: "Patron7370",
+          handle: "May 17, 2026",
           title: "Amazing",
           copy: "You need to get this app and after this nothing else."
         },
         {
           initials: "P",
           name: "PreetamAIPro",
-          handle: "PreetamAIPro",
+          handle: "Jan 11, 2026",
           title: "It is serious",
           copy: "I never thought AI and astrology can reveal so many things which even the celebrity astrologers have failed so far."
         }
@@ -55,21 +55,21 @@ export default {
         {
           initials: "C",
           name: "Charu anime \"Queen\"",
-          handle: "Charu anime \"Queen\"",
+          handle: "15 May 2026",
           title: "Spiritual bestie vibes",
           copy: "I love this app so much. It's giving me spiritual bestie vibes. I love it."
         },
         {
           initials: "H",
           name: "Heather",
-          handle: "Heather",
+          handle: "22 May 2026",
           title: "Learn yourself",
           copy: "Great place to learn yourself, your sign, and relationships. Love it."
         },
         {
           initials: "R",
           name: "Reabetswe Nteo",
-          handle: "Reabetswe Nteo",
+          handle: "7 May 2026",
           title: "Makes me feel good",
           copy: "I'm so in love with Staia. It makes me feel good whenever I feel down."
         }
@@ -80,21 +80,21 @@ export default {
         {
           initials: "T",
           name: "Tebogo Rapoo",
-          handle: "Tebogo Rapoo",
+          handle: "1 June 2026",
           title: "On point",
           copy: "It's on point. Great app, you won't be disappointed."
         },
         {
           initials: "R",
           name: "RATHI SASIKUMAR",
-          handle: "RATHI SASIKUMAR",
+          handle: "16 April 2026",
           title: "Understand yourself",
           copy: "Great application to understand yourself."
         },
         {
           initials: "C",
           name: "Cheshta Modi",
-          handle: "Cheshta Modi",
+          handle: "20 April 2026",
           title: "Life saver",
           copy: "The most important thing I ever heard about. I have no words for these. Just try once, you get it."
         }
